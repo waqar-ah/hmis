83cd79a2000ca2dc821a9e65b4ccf30c60ecc6bc
Waqar ahmad <waqar.ah.awan@gmail.com>
2022-02-13 02:30:56 +0500

    update

---
+++ rooms-management/src/main/java/com/waqar/hmis/roomsmanagement/RoomsManagementApplication.java
@@ -0,0 +1,13 @@
+package com.waqar.hmis.roomsmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoomsManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoomsManagementApplication.class, args);
+	}
+
+}
